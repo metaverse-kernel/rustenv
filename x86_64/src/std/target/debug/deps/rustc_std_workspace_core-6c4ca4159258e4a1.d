f8d0d53a23b5a91848d8f6491122b69f79994537
pointer-to-bios <pointer-to-bios@outlook.com>
2024-01-16 23:47:22 +0800

    初始化并添加x86架构的rust环境

---
+++ x86_64/src/std/target/debug/deps/rustc_std_workspace_core-6c4ca4159258e4a1.d
@@ -0,0 +1,5 @@
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/deps/rustc_std_workspace_core-6c4ca4159258e4a1.rmeta: /home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-core-1.0.0/src/lib.rs
+
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/deps/rustc_std_workspace_core-6c4ca4159258e4a1.d: /home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-core-1.0.0/src/lib.rs
+
+/home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-core-1.0.0/src/lib.rs:
