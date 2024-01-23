a7d79f8d3d865c93dad38796530e6a4dc53a0377
pointer-to-bios <pointer-to-bios@outlook.com>
2024-01-24 02:25:15 +0800

    只使用core crate

---
+++ x86_64/src/std/target/debug/build/std-7bc75d5e28f5d2b7/build_script_build-7bc75d5e28f5d2b7.d
@@ -0,0 +1,5 @@
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/build/std-7bc75d5e28f5d2b7/build_script_build-7bc75d5e28f5d2b7: build.rs
+
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/build/std-7bc75d5e28f5d2b7/build_script_build-7bc75d5e28f5d2b7.d: build.rs
+
+build.rs:
---
+++ x86_64/src/std/target/debug/deps/rustc_std_workspace_alloc-0b9a1dc20c145ca4.d
@@ -0,0 +1,5 @@
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/deps/rustc_std_workspace_alloc-0b9a1dc20c145ca4.rmeta: /home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-alloc-1.0.0/src/lib.rs
+
+/home/pointer-to-bios/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/std/target/debug/deps/rustc_std_workspace_alloc-0b9a1dc20c145ca4.d: /home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-alloc-1.0.0/src/lib.rs
+
+/home/pointer-to-bios/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-std-workspace-alloc-1.0.0/src/lib.rs:
